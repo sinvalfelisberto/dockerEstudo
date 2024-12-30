825e41ceb690e62c8332416c0084746278498443
sinvalfelisberto <sinvalfelisberto@gmail.com>
2024-12-30 14:13:20 -0300

    "alteração dos param de query"

--- project/database/index.php
+++ project/database/index.php
@@ -10,7 +10,7 @@
     }
     echo "Connected successfully";
 
-    $res = $conn->query("SELECT * FROM tb_users");
+    $res = $conn->query("SELECT username, email, creation_timestamp FROM tb_users");
 
     echo "<pre>";
     print_r($res->fetch_all());
